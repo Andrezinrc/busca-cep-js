dc4941962d2e304b7e4e3830344d7079ca10b07e
andrezinrc <andrem.33333@gmail.com>
2023-03-29 17:51:44 -0300

    .

--- script.js
+++ script.js
@@ -4,21 +4,23 @@ async function buscarCep(){
     const input = $("#input").val()
 
     try{
-        
+
         if(input === ""){
-            alert("Preencha o Campo")
+            $("#text").text("Preencha o campo")
         }
         else{
-            const resposta = await fetch(API + input + "/json")
+            const resposta = await fetch(API + input + "/json/")
             const data = await resposta.json()
 
             $("#cep").text("CEP: " + data.cep)
-            $("#logradouro").text("RUA: " + data.logradouro)
-            $("#bairro").text("BAIRRO: " + data.bairro)
-            $("#localidade").text("CIDADE: " + data.localidade + "/" + data.uf)
+            $("#logradouro").text("Rua: " + data.logradouro)
+            $("#bairro").text("Bairro: " + data.bairro)
+            $("#localidade").text("Cidade: " + data.localidade + "/" + data.uf)
+
         }
     }
     catch(erro){
-        console.log("Deu ero nessa merda" + erro)
+        $("#text").text("Ops Houve um erro :(")
+        console.log(erro)
     }
 }
