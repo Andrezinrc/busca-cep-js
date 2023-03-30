673dee1a5d0e71b38ff20843ee28ec42c71ec723
andrezinrc <andrem.33333@gmail.com>
2023-03-30 19:59:03 -0300

    >

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
-var API = "https://viacep.com.br/ws/";
+const API = "https://viacep.com.br/ws/";
 
-async function buscarCep(){
-    const input = $("#input").val()
+const buscarCep = async () => {
+    const input = $("#input").val();
 
-    try{
+    try {
+        const cleanedInput = input.replace(/\D/g, '');
+        if (cleanedInput === "") {
+            $("#text").text("CEP Inválido");
+            setTimeout(() => {
+                location.reload();
+            }, 2000);
+        } else {
+            const resposta = await fetch(API + cleanedInput + "/json/");
+            const data = await resposta.json();
 
-        if(input === ""){
-            $("#text").text("Preencha o campo")
+            if (!data.cep) {
+                $("#text").text("CEP Inválido");
+                setTimeout(() => {
+                    location.reload();
+                }, 2000);
+            } else {
+                $("#input").val("");
+                $("#cep").text("CEP: " + data.cep);
+                $("#logradouro").text("Rua: " + data.logradouro);
+                $("#bairro").text("Bairro: " + data.bairro);
+                $("#localidade").text("Cidade: " + data.localidade + "/" + data.uf);
+            }
         }
-        else{
-            const resposta = await fetch(API + input + "/json/")
-            const data = await resposta.json()
-
-            $("#cep").text("CEP: " + data.cep)
-            $("#logradouro").text("Rua: " + data.logradouro)
-            $("#bairro").text("Bairro: " + data.bairro)
-            $("#localidade").text("Cidade: " + data.localidade + "/" + data.uf)
-
-        }
+    } catch (erro) {
+        console.log("Houve um erro" + erro);
+        $("#text").text("Houve um erro :(");
+        setTimeout(() => {
+            location.reload();
+        }, 2000);
     }
-    catch(erro){
-        $("#text").text("Ops Houve um erro :(")
-        console.log(erro)
-    }
-}
+};
